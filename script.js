c5cf18c6d6d3b8af8218ba0bd8dd224d5d309971
marinademinescu <demi.marina@yahoo.it>
2025-10-14 15:39:24 +0200

    creato menu hamburger

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 // Dark / Light mode
-const toggle = document.getElementById('dark-mode');
+const toggle = document.getElementById('dark-mode')
 toggle.addEventListener('change', () => {
-  document.body.classList.toggle('dark-mode', toggle.checked);
-});
+  document.body.classList.toggle('dark-mode', toggle.checked)
+})
 
 
 
-const videoTrigger = document.getElementById('video-trigger');
-const video = document.querySelector('.hero-video');
+const video = document.querySelector('.hero-video')
 
-videoTrigger.addEventListener('click', () => {
+video.addEventListener('click', () => {
   if (video.classList.contains('active')) {
-  video.classList.add('active');
-    video.play();
+  video.classList.add('active')
+    video.play()
   } else {
 
-     video.pause();
-    video.currentTime = 0;
-    video.classList.remove('active');
+     video.pause()
+    video.currentTime = 0
+    video.classList.remove('active')
   }
-});
+})
+
+
+
+
+// Hamburger toggle
+const hamburger = document.getElementById("hamburger")
+const mobileNav = document.getElementById("mobile-menu-wrapper")
+
+hamburger.addEventListener("click", () => {
+  mobileNav.classList.toggle("open")
+  hamburger.classList.toggle("is-active")      // per trasformare l'icona in X
+ hamburger.innerHTML= hamburger.classList.contains("is-active")
+  ? "<i class='bx bx-x'></i>" 
+    : "<i class='bx bx-menu'></i>"
+})
+
+// Mobile dropdown toggle
+const mobileDropdownLinks = document.querySelectorAll(".mobile-menu-wrapper .submenu-mobile > a")
+
+mobileDropdownLinks.forEach(link => {
+  link.addEventListener("click", (e) => {
+    const parent = link.parentElement
+    if (parent.querySelector("ul")) {
+      e.preventDefault()
+      parent.classList.toggle("open")
+    }
+  })
+})
+
+
